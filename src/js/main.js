2dd6e3aaee5837e04d874e84e090c0630d075a64
Stan Chashchnikov <chashchnikov@gmail.com>
2016-04-07 01:31:43 +0300

    Исправил ссылку на файл с данными

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,11 @@ import Menu from 'menu';
 
 var menu = new Menu($('.menu'));
 
+
 $.ajax({
-  url: '/js/menu-content.json',
-  dataType: 'json'
-}).done((data) => {
-  menu.addItem({name: 'Test', url: '#'}, data);
+  url: 'js/menu-content.json',
+  dataType: 'json',
+  success: (data) => {
+    menu.addItem({name: 'Test', url: '#'}, data);
+  }
 });
